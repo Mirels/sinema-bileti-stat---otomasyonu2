d8b16928ddff92c222bd03906626d7f90d39fac3
Mirels <97889152+Mirels@users.noreply.github.com>
2023-06-18 17:31:40 +0300

    print preview ve print alma özelliği eklenmiştir

---
+++ görsel programlama/İrem/proje/Proje1/Proje1/Helpers/Helper.cs
@@ -0,0 +1,72 @@
+﻿using Proje1.Enums;
+using Proje1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje1.Helpers
+{
+    class Helper
+    {
+        public static List<Movie> createMovies()
+        {
+            string basePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            return new List<Movie>()
+            {
+                new Movie()
+                {
+                    movieName = "Karayip Korsanları",
+                    category = Category.gerilim,
+                    minute = "1 Saat 20 Dakika",
+                    price = 60,
+                    picturePath = basePath + "/pirates.jpeg"
+                },
+                   new Movie()
+                {
+                    movieName = "Island",
+                    category = Category.macera,
+                    minute = "2 Saat 10 Dakika",
+                    price = 60,
+                    picturePath = basePath + "/island.jpg"
+                },
+                new Movie()
+                {
+                    movieName = "Looper",
+                    category = Category.bilimKurgu,
+                    minute = "2 Saat 55 Dakika",
+                    price = 20,
+                    picturePath = basePath + "/looper.jpg"
+                },
+                new Movie()
+                {
+                    movieName = "Matrix",
+                    category = Category.bilimKurgu,
+                    minute = "3 Saat 1 Dakika",
+                    price = 30,
+                    picturePath = basePath + "/matrix.jpg"
+                },
+                new Movie()
+                {
+                    movieName = "Mumya",
+                    category = Category.komedi,
+                    minute = "2 Saat 40 Dakika",
+                    price = 50,
+                    picturePath = basePath + "/mummy.jpg"
+                },
+
+             new Movie()
+                {
+                    movieName = "Pulp Fiction",
+                    category = Category.bilimKurgu,
+                    minute = "3 Saat 30 Dakika",
+                    price = 200,
+                    picturePath = basePath + "/pulp.jpg"
+                },
+
+
+            };
+        }
+    }
+}
---
+++ görsel programlama/İrem/proje/Proje1/Proje1/Model/Sales.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace Proje1.Model
+{
+    public class Sales 
+    {
+        public Sales()
+        {
+            KesimTarih = DateTime.Now.ToString();
+        }
+        public virtual int Numara { get; set; }
+        public virtual string KesimTarih { get; set; }
+        public virtual decimal Ücret { get; set; }
+        public virtual int Adet { get; set; }
+
+        public virtual string Seans { get; set; }
+
+        public virtual string MüsteriBilgi { get; set; }
+
+        public virtual string Koltuk { get; set; }
+
+        public override string ToString()
+        {
+            Movie movie = new Movie();
+            return $"{movie.movieName} Adlı Filmin {Seans} Seansına {Adet} Adet Bilet Kesilmiştir. Toplam Tutar {Ücret} ₺ || Satın" +
+                $"Alınma Tarihi = {KesimTarih}";
+        }
+    }
+
+    public class SalesMapping : ClassMapping<Sales>
+    {
+        public SalesMapping()
+        {
+            Table("Sales");
+            Id(x => x.Numara, m => m.Generator(Generators.Native));
+            Property(x => x.KesimTarih, x => x.NotNullable(true));
+            Property(x => x.Ücret, x => x.NotNullable(true));
+            Property(x => x.Adet, x => x.NotNullable(true));
+            Property(x => x.Seans, x => x.NotNullable(true));
+            Property(x => x.MüsteriBilgi, x => x.NotNullable(false));
+            Property(x => x.Koltuk, x => x.NotNullable(false));
+        }
+    }
+}
